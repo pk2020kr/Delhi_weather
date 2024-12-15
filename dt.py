d449da5e03a679ad1b4e9b6d3bd241d17d69382f
Prabhat <pk2020kr@gmail.com>
2024-12-15 10:13:09 +0530

    Group

--- dt.py
+++ dt.py
@@ -17,3 +17,31 @@ df.head(2)
 df = pd.read_csv('testsetf.csv')
 data = pd.DataFrame(list(df['_tempm']), index=df['datetime_utc'], columns=['temp'])
 data.head(4)
+
+
+
+#Group (month by month) and (year by year)
+df = pd.read_csv('testsetf.csv')   # Load the dataset
+
+# Load the dataset, parsing the 'datetime_utc' column as datetime objects
+df = pd.read_csv("testsetf.csv", parse_dates= ["datetime_utc"])
+
+# Create a new DataFrame 'data' with the '_tempm' column as the temperature values and 'datetime_utc' as the index
+data = pd.DataFrame(list(df['_tempm']), index=df['datetime_utc'], columns=['temp'])
+data.head(4)
+
+
+avg_month = data.resample('m').mean()  # Resampling data with date frequency (month by month)
+avg_month.head(4)
+
+avg_year = data.resample('y').mean()  # Resampling data with date frequency (year by year)
+avg_year.head(4)
+
+avg_year = avg_month.resample('y').mean()  # resampling data with date frequency (year by year)
+avg_year.head(4)                           # This and above are same result but this reduce timecomplexicity 
+
+y = avg_year['1998':'2002']   # y = avg_year['1997-12-31':'2016-12-31']   # slicing
+y.head()
+
+y = avg_year['1998':'2010':4]  # y = avg_year['1997-12-31':'2016-12-31': Gap]
+y
