a5df065585aea09b68d52880e6143868b0eed3a0
Prabhat <pk2020kr@gmail.com>
2024-12-15 09:45:29 +0530

    time and temp

--- dt.py
+++ dt.py
@@ -7,3 +7,13 @@ import seaborn as sns
 # Read .csv file
 df = pd.read_csv('testsetf.csv') #df = pd.read_csv("/kaggle/input/delhi-weather-data/testset.csv") sourse from where I bring data        
 df.head()                        # Display the rows of the dataframe
+
+# Set the Time column as the index of the dataframe
+df.set_index(['datetime_utc'], inplace=True) # Set the 'datetime_utc' column as the index of the dataframe
+df.head(2)
+
+# Fatch only 2 set time & temp
+# taking only temperature feature as values and datetime feature as index in the dataframe 
+df = pd.read_csv('testsetf.csv')
+data = pd.DataFrame(list(df['_tempm']), index=df['datetime_utc'], columns=['temp'])
+data.head(4)
